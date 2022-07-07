776773a0d9437c995fc88c14580621aaa6afa9a5
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-07-07 15:54:02 -0400

    finished additional requirements and bonus mission

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   width = 0;
   message = 'Space shuttle ready for takeoff!';
 
+  takeOffEnabled: Boolean = true;
+
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
@@ -23,23 +25,25 @@ export class AppComponent {
     }
   }
 
-  handleLanding() {
+  handleLanding(rocketImage) {
     let result = window.confirm("The shuttle is landing.Landing gear engaged.");
     if (result) {
       this.color = 'green';
       this.height = 0;
       this.width = 0;
       this.message = 'The shuttle has landed';
+      rocketImage.style.bottom = '0px';
     }
   }
 
-  handleAbortMission() {
+  handleAbortMission(rocketImage) {
     let result = window.confirm("Are you sure you want to abort?");
     if (result) {
       this.color = 'red';
       this.height = 0;
       this.width = 0;
       this.message = 'Mission aborted';
+      rocketImage.style.bottom = '0px';
     }
   }
 
@@ -47,11 +51,11 @@ export class AppComponent {
     if (direction === 'right') {
       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
       rocketImage.style.left = movement;
-      // this.width = this.width + 10000;
+      this.width = this.width + 10000;
     } else if (direction === 'left') {
       let movement = parseInt(rocketImage.style.left) + -10 + 'px';
       rocketImage.style.left = movement;
-      // this.width = this.width + 10000;
+      this.width = this.width - 10000;
     }else if (direction === 'up') {
       let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
       rocketImage.style.bottom = movement;
@@ -62,4 +66,19 @@ export class AppComponent {
       this.height = this.height - 10000;
     }
   }
+    tooClose(){
+      if (this.height > 340000 || this.height < 5 || this.width > 250000 || this.width < 0){
+        this.color = 'orange';
+      }else{
+          this.color = 'blue';
+      }
+    }
+    
+
+    offThePage () {
+      if (this.height > 350000 || this.height < -10000 || this.width > 300000 || this.width < -10000){
+          return true;
+      }
+    }
 }
+

76fc9169cea76a0a312ac5b3b283fd59e3385d01
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-07-07 11:16:58 -0400

    completed control button event handlers

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,34 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLanding() {
+    let result = window.confirm("The shuttle is landing.Landing gear engaged.");
+    if (result) {
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'The shuttle has landed';
+    }
+  }
+
+  handleAbortMission() {
+    let result = window.confirm("Are you sure you want to abort?");
+    if (result) {
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission aborted';
+    }
+  }
 }
